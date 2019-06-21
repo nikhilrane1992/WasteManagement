af3746d8d4377aa856fe9f2ba05d79428625ee79
Nikhil Rane <--add>
2019-06-21 12:27:17 +0530

    Added base path in wsgi file

--- WasteManagement/WasteManagement/wsgi.py
+++ WasteManagement/WasteManagement/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WasteManagement.settings")
 
 application = get_wsgi_application()
